6effbd689720c92c2f6d1dfa65fd64556667ad5e
김현빈 <96505736+khv9786@users.noreply.github.com>
2023-05-20 15:26:05 +0900

    [unrated] Title: 추억 점수, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -1,26 +1,13 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
-        HashMap <String,Integer> map = new HashMap<>();
-        
-        for(int i = 0; i<name.length; i++){
-        map.put(name[i],yearning[i]);
-        }
-        
-        for(int i = 0; i<photo.length; i++){
-            int score = 0;
-            for(int j = 0; j<photo[i].length; j++){
-                String names = photo[i][j];
-                if(map.containsKey(names)){
-                    score +=map.get(names);
+        for(int i = 0; i < photo.length; i++){
+            for(int j = 0; j < photo[i].length; j++){
+                for(int k = 0; k < name.length; k++){
+                    if(photo[i][j].equals(name[k])) answer[i] += yearning[k];
                 }
-            answer[i] = score;
             }
         }
-        
-        
         return answer;
     }
 }
