20d1394aa91e41fddac259b9efc09b147b5c2af1
김현빈 <96505736+khv9786@users.noreply.github.com>
2023-06-01 23:19:52 +0900

    [unrated] Title: 주사위 게임 3, Time: 0.55 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int a, int b, int c, int d) {
+        int[] dice = {a, b, c, d};
+        Arrays.sort(dice);
+
+        if (dice[0] == dice[3]) { // 4개 동일할때
+            return 1111 * dice[3];
+            
+        } else if (dice[0] == dice[2] || dice[1] == dice[3]) { // 세개 같을때
+            return (int) Math.pow(dice[1] * 10 + (dice[0] + dice[3] - dice[1]), 2);
+            
+        } else if (dice[0] == dice[1] && dice[2] == dice[3]) { // 둘둘 같을 때
+            return (dice[0] + dice[3]) * Math.abs(dice[3] - dice[0]);
+            
+        } else if (dice[0] == dice[1]) { // 둘 하나하 경우
+            return dice[2] * dice[3];
+            
+        } else if (dice[1] == dice[2]) {
+            return dice[0] * dice[3];
+            
+        } else if (dice[2] == dice[3]) {
+            return dice[0] * dice[1];
+            
+        } else if (dice[3] == dice[0]) {
+            return dice[1] * dice[2];
+        }else {
+            return dice[0];
+        }
+    }
+}
