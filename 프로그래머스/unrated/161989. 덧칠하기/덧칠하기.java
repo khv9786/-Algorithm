cdcbd49a3bb70514af25c067d3ba323f61a1f877
김현빈 <96505736+khv9786@users.noreply.github.com>
2023-06-13 13:24:38 +0900

    [unrated] Title: 덧칠하기, Time: 3.65 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -3,29 +3,24 @@ class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
         int cnt = 0;
-        int emp = 0;
         
         int[] arr = new int[n];
-        Arrays.fill(arr, 1);
-        
+        Arrays.fill(arr, 1); // 배열 전부 1로 채우기.
+    
         for(int i = 0; i < section.length; i++){
-            arr[section[i]-1] = 0;
+            arr[section[i]-1] = 0; // 빈곳은 0 으로 변경.
         }
         
-        for(int i = 0; i < arr.length; i++){
-
-          
+        for(int i = 0; i < arr.length; i++){ // 배열을 돌면서 빈곳 체크 후 m이면 횟수 증가.
             if(arr[i] == 0 || cnt > 0){
                 cnt ++;
-            }
-                if(cnt == m){
+            }if(cnt == m){
                     answer ++;
                     cnt = 0;
-                }  
-        } if(cnt > 0){
+            }  
+        }if(cnt > 0){ // m만큼 안채워졌어도 칠해야한다면 횟수 증가.
             answer ++;
         }
-        
         return answer;
     }
 }
