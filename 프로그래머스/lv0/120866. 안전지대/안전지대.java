99d6b58c7d479423b055a663ffe026e57f7d7a71
김현빈 <96505736+khv9786@users.noreply.github.com>
2023-06-18 12:39:03 +0900

    [level 0] Title: 안전지대, Time: 0.05 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int solution(int[][] board) {
+        int n = board.length;
+        int answer = n*n;
+
+        int[] dx = {-1, 0, 1, -1, 1, -1, 0, 1}; // 주변 범위 설정.
+        int[] dy = {-1, -1, -1, 0, 0, 1, 1, 1};
+
+        boolean[][] cnt = new boolean[n][n]; // 확인용 cnt 생성
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < 8; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+                    if (board[i][j] == 1) {
+                        cnt[i][j] = true;
+                    }
+
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 1) {
+                        cnt[i][j] = true;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < cnt.length; i++) {
+            for (int j = 0; j < cnt.length; j++) {
+                if (cnt[i][j]) {
+                    answer--;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
