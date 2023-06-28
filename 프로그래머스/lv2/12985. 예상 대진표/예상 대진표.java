27c152c2dd3f617f22899fa362902b960246663a
김현빈 <96505736+khv9786@users.noreply.github.com>
2023-06-28 12:56:50 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 82.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int a, int b) {
+        int answer = 0;
+
+        while (true) {
+            if ( a==b )
+                break;
+            a = (a+1) / 2;
+            b = (b+1) / 2;
+            
+            
+            answer++;
+            
+        }
+
+        return answer;
+    }
+}
