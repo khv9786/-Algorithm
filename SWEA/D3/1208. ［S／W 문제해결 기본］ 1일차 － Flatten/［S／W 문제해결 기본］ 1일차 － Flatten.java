9e52df9122b6a948e06d4e4133b3c400b812ef2c
김현빈 <96505736+khv9786@users.noreply.github.com>
2023-08-06 13:56:05 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 181 ms, Memory: 27,076 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -7,23 +7,24 @@ class Solution
 	{
 
 		  Scanner sc = new Scanner(System.in);
-	        for(int i = 0; i<10; i++) {
+	        for(int i = 0; i<10; i++) { 		//10 tc
 	            int N = sc.nextInt();
 	            int [] arr = new int [100];
-	            for(int k = 0; k < 100; k++) {            	
+	            for(int k = 0; k < 100; k++) {  // 가로길이 100   	
 	            	arr[k]= sc.nextInt();           	
 	            }
 	          //logic
 	            for(int k = 0; k < N; k++) {
-	            	Arrays.sort(arr);
-	            	if(arr[0] == arr[arr.length-1]) {
+	            	Arrays.sort(arr);			// 먼저 정렬 후
+	            	if(arr[0] == arr[arr.length-1] && arr[1] == arr[arr.length-2]) { 
+	            		// 평탄하다면 break;
 	            		break;
 	            	}else {
-	            		arr[0] ++;
-		            	arr[arr.length-1] --;
+	            		arr[0] ++;				//최저점 +1
+		            	arr[arr.length-1] --;	//최고점 -1
 	            	}
 	            }
-	            Arrays.sort(arr);
+	            Arrays.sort(arr);				// 정렬 후 출력.
 	          	int answer = arr[arr.length-1]-arr[0];
 	          	
 	          	//output
