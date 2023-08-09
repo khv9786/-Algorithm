4f854f1fe65705d1b8fef0ccfcfe8a301426d6c9
김현빈 <96505736+khv9786@users.noreply.github.com>
2023-08-09 15:57:24 +0900

    [unrated] Title: l로 만들기, Time: 7.79 ms, Memory: 81.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String myString) {
+        StringBuilder answer = new StringBuilder();
+        
+        for (int i = 0; i < myString.length(); i++) {
+            if (myString.charAt(i) - 'a' > 'l' - 'a') {
+                answer.append(myString.charAt(i));
+            } else {
+                answer.append('l');
+            }
+        }
+        
+        return answer.toString();
+    }
+}
