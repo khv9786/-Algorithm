50bb9fa9e25bd377ed53031712fc86040ca3f5b0
김현빈 <96505736+khv9786@users.noreply.github.com>
2023-09-19 01:09:18 +0900

    [unrated] Title: 배열의 원소 삭제하기, Time: 0.03 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
@@ -3,19 +3,14 @@ class Solution {
 
     public ArrayList solution(int[] arr, int[] delete_list) {
         ArrayList<Integer> answer = new ArrayList<>();
-        // 가변적이기 때문에 배열이 아닌 list사용
         
-        for(int i = 0; i < arr.length; i++){ // 먼저 기존 배열만큼 반복
-            boolean chk = true; // 겹치는게 있는지 확인 위한 boolean
-            for(int j = 0; j<delete_list.length; j++) 
-                // delete에 있는 요소 모두 확인
-                if(arr[i] == delete_list[j]){ // 만약 있다면
-                    chk = false; // chk boolean 변경
-                    break;      // 불필요한 반복 X위해 break;
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j<delete_list.length; j++)
+                if(arr[i] == delete_list[j]){
+                    break;
+                }else{
+                    answer.add(arr[i]);
                 }
-            if(chk){ // 만약 chk가 true라면 -> delete랑 안겹쳤다면
-                answer.add(arr[i]); // answer 리스트에 add해주기
-            }
         }
         
         
