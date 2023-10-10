b67a3ca0e1eb3ee4eb84f59cb91f75a3257c3368
김현빈 <96505736+khv9786@users.noreply.github.com>
2023-10-11 00:38:26 +0900

    [Silver II] Title: 연속합, Time: 824 ms, Memory: 108164 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main { // 1912. 연속합 35%
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.nextLine();	
+		int[] arr = new int[n];
+		// Input
+		for (int num = 0; num < n; num++) {			
+			arr[num] = sc.nextInt();			
+		}
+		int [] dp = new int [n];
+		dp[0] = arr[0];
+		int max = Integer.MIN_VALUE;
+		// Logic
+		for(int i = 1; i<n; i++) { // i별 최대값을 저장.
+			dp[i] = Math.max(arr[i], dp[i-1] + arr[i]); // i-1 최대값 + 다음값이랑 현재값 비교
+
+		}
+		  for (int i : dp) {
+	            max = Math.max(i, max);
+	        }
+
+		// Output
+		System.out.println(max);
+
+		sc.close();
+	}// main
+}// solution
