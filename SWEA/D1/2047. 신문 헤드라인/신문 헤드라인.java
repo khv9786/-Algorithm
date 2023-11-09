d43fadf08a9c049c3a736cee861e3777c81777b0
김현빈 <96505736+khv9786@users.noreply.github.com>
2023-11-09 19:09:48 +0900

    [D1] Title: 신문 헤드라인, Time: 127 ms, Memory: 19,956 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution { // 2046. 스탬프 찍기 D1
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String s = sc.nextLine();
+		s = s.toUpperCase();
+		System.out.printf(s);
+
+	}
+}
