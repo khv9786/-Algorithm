cdbb9a9e2a898f909effc16ec4b4078cfa7ee3e6
김현빈 <96505736+khv9786@users.noreply.github.com>
2023-11-11 00:58:02 +0900

    [Silver IV] Title: 바닥 장식, Time: 216 ms, Memory: 17908 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.java
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.java
@@ -1,11 +1,14 @@
 import java.util.*;
 
 public class Main { // 1388번 백준 바닥장식 S4
+	// dfs를 통해 방문하지 않은 곳을 dfs함수 적용.
+	// -에서 다음 행이 -라면 재귀로 진행.
+	// | 이후 같은 열이 | 라면 재귀로 진행.
 	public static char[][] room;
 	public static boolean[][] visited;
 	public static int answer, N, M;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args)  {
 		// input
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
@@ -24,14 +27,18 @@ public class Main { // 1388번 백준 바닥장식 S4
 
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
+				// 방문하지 않았다면
 				if (!visited[i][j]) {
+					// - 인경우 
 					if (room[i][j] == '-') {
 						dfs_row(i, j);
 					}
+					// | 인 경우
 					if (room[i][j] == '|') {
 						dfs_column(i, j);
 					}
-					answer++;
+					// dfs에 한번 넣으면 무조건 조각 하나이므로 ++
+					answer += 1;
 				}
 			}
 		}
@@ -40,21 +47,26 @@ public class Main { // 1388번 백준 바닥장식 S4
 	}
 
 	public static void dfs_row(int r, int c) {
+		// 만약 | 라면 연결이 끊겼으므로 종료.
 		if (room[r][c] == '|') {
 			return;
 		}
+		// 방문했으므로 체크.
 		visited[r][c] = true;
+		// 범위가 넘어가지 않는다면 다음으로 진행.
 		if (c + 1 < M) {
 			dfs_row(r, c + 1);
 		}
 	}
 
 	public static void dfs_column(int r, int c) {
+		// 만약 - 라면 연결이 끊겼으므로 종료
 		if (room[r][c] == '-') {
 			return;
 		}
+		// 방문했으므로 체크.
 		visited[r][c] = true;
-
+		// 범위가 넘어가지 않는다면 다음으로 진행.
 		if (r + 1 < N) {
 			dfs_column(r + 1, c);
 		}
