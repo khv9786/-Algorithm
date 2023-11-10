3e54025207bf15b5e63bd94786a262bc0087a1a6
김현빈 <96505736+khv9786@users.noreply.github.com>
2023-11-10 16:27:47 +0900

    [Silver IV] Title: 자전거 묘기, Time: 664 ms, Memory: 54104 KB -BaekjoonHub

--- 백준/Silver/25706. 자전거 묘기/자전거 묘기.java
+++ 백준/Silver/25706. 자전거 묘기/자전거 묘기.java
@@ -1,16 +1,20 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args)throws IOException  {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
         int arr[] = new int[N + 1];
         int[] dp = new int[N + 1];
-
-        for (int i = 1; i <= N; i++) {
-            arr[i] = sc.nextInt();
-        }
         dp[N] = 1;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
         for (int i = N - 1; i > 0; i--) {
             if (arr[i] != 0) {
                 int temp = i + arr[i] + 1;
@@ -20,8 +24,11 @@ public class Main {
             }
         }
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= N; i++) {
-            System.out.print((i == N) ? dp[i] : dp[i] + " ");
+            sb.append((i == N) ? dp[i] : dp[i] + " ");
         }
+
+        System.out.println(sb.toString());
     }
 }
