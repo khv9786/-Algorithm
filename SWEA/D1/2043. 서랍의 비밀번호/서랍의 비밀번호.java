a502c0567481c28bae16636840e467c7397fe000
김현빈 <96505736+khv9786@users.noreply.github.com>
2023-11-14 21:38:36 +0900

    [D1] Title: 서랍의 비밀번호, Time: 122 ms, Memory: 20,524 KB -BaekjoonHub

---
+++ SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class Solution { // swea 2050. 알파벳을 숫자로 변환
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);	
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a-b + 1);
+
+	}
+
+}
