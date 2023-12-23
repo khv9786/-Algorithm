89ae3bd916411ffca0046ec7da4c2a447d2dda08
김현빈 <96505736+khv9786@users.noreply.github.com>
2023-12-23 16:24:01 +0900

    [Bronze II] Title: 숫자, Time: 144 ms, Memory: 14516 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {// 10093번 숫자 B2 23% 길래..
+	// 구현 뭐야 나도 틀렸네 방심하지 맙시당. 2분컷하느라 호
+	static long A, B;
+	static long answer, cnt;
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		A = Long.parseLong(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		long Big = Math.max(A, B);
+		long Small = Math.min(A, B);
+
+		cnt = Big - Small - 1;
+
+		sb = new StringBuilder();
+		if(A == B) {
+			sb.append(0);
+		}else {
+			sb.append(cnt);
+			sb.append("\n");
+			for (long i = Small + 1; i < Big; i++) {
+				sb.append(i + " ");
+			}
+		}
+		
+
+		System.out.println(sb);
+	}
+}
