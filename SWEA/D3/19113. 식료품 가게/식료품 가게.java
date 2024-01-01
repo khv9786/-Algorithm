74d87e09e6f73b87fe286b286e31673e49e1b0ab
김현빈 <96505736+khv9786@users.noreply.github.com>
2024-01-01 16:39:16 +0900

    [D3] Title: 식료품 가게, Time: 276 ms, Memory: 53,700 KB -BaekjoonHub

---
+++ SWEA/D3/19113. 식료품 가게/식료품 가게.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int Tc = 1; Tc <= T; Tc++) {
+            StringBuilder sb = new StringBuilder();
+            int N = sc.nextInt();
+
+            Long[] answer = new Long[N]; 
+            long[] num = new long[N * 2];
+            boolean[] used = new boolean[N * 2];
+
+            for (int i = 0; i < 2 * N; i++) {
+                long temp = sc.nextLong();
+                num[i] = temp;
+                used[i] = true;
+            }
+            
+            int idx = 0; 
+            for (int i = 0; i < 2 * N; i++) {
+                if (used[i]) {
+                    long cur = num[i];
+                    for (int j = i; j < 2 * N; j++) {
+                        long before = cur * 4 / 3;
+                        if (num[j] == before && used[j]) {
+                            answer[idx] =  cur; 
+                            used[j] = false;
+                            used[i] = false;
+                            idx++;
+                            break;
+                        }
+                    }
+                }
+            }
+            
+            Arrays.sort(answer);
+            sb.append("#" + Tc + " ");
+            for (Long a : answer) {
+                sb.append(a + " ");
+            }
+            System.out.println(sb);
+        }
+    }
+}
