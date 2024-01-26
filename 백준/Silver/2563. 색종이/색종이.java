059b53d4da05a235f243e5ecf7a255729680318e
김현빈 <96505736+khv9786@users.noreply.github.com>
2024-01-26 14:40:14 +0900

    [Silver V] Title: 색종이, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,33 +1,37 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {// Boj_2563_색종이
-	static int N, M, answer;
+public class Main { // Boj_2563_색종이
+	public static int N, x, y, answer;
+	public static int[][] map;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		N = Integer.parseInt(st.nextToken()); // 색종이 수
-		
-		int arr [] = new int [N];
-		int paper [][] = new int [100][100];
-		for(int i = 0; i<N; i++) {
+		N = Integer.parseInt(st.nextToken());
+		map = new int[101][101];
+		int length = 10;
+		for (int tc = 0; tc < N; tc++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			for(int j = a; j<a+10; j++) {
-				for(int k = b; k<b+10; k++) {
-					paper[j][k] = 1;
+			x = Integer.parseInt(st.nextToken());
+			y = Integer.parseInt(st.nextToken());
+			for(int i = x; i<x+10; i++) {
+				for(int j = y; j<y+10; j++) {
+					map[i][j] = 1;
 				}
 			}
 		}
+
 		
-		for(int i = 0; i<100; i++) {
-			for(int j = 0; j<100; j++) {
-				answer += paper[i][j];
+		for(int i = 0; i<101; i++) {
+			for(int j = 0; j<101; j++) {
+				if(map[i][j] == 1) {
+					answer ++;
+				}
 			}
 		}
+
 		System.out.println(answer);
+
 	}
 }
