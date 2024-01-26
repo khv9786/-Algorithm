04d50749bc7e6eda55194b0c52c76d811060ba76
김현빈 <96505736+khv9786@users.noreply.github.com>
2024-01-26 14:57:28 +0900

    [Silver III] Title: 계단 오르기, Time: 120 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,32 +1,37 @@
 import java.util.*;
+import java.io.*;
 
-public class Main { // 백준 2579 계단 오르기 실3
+public class Main { // 2579_계단오르기
+	// dp
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = new StringTokenizer(br.readLine());
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int answer = 0;
-		int N = sc.nextInt();
-		int  arr[] = new int[N + 1];
-		int dp[] = new int[N + 1];
-		for (int i = 1; i <= N; i++) {
-			arr[i] = sc.nextInt();
+		int T = Integer.parseInt(br.readLine());
+		int size = T;
+		
+		if (T <= 2) {
+			size = 2;
 		}
 		
+		int[] stair = new int[size + 1];
+		int[] dp = new int[size + 1];
+		// 계단은 1칸을 가거나, 2칸을 간다. 3연속은 불가능
+		// dp[4] = 2 -> 4 또는 3 -> 4 존재 단 3 -> 4의 경우 1 3 만 가능.
+		// dp[i] = Math.max(dp[i-2],dp[i-3]+stair[i-1]) + stair[i];
+		for (int i = 1; i <= T; i++) {
+			int N = Integer.parseInt(br.readLine());
+			stair[i] = N;
+		}
+
 		dp[0] = 0;
-		if(N >= 1) {
-			dp[1] = arr[1];
-		}
-		if(N>=2) {
-			dp[2] = arr[1] + arr[2];
-		}
-		if(N>=3) {
-			dp[3] = Math.max((arr[1] + arr[3]), (arr[2] + arr[3]));
-		}
-		
+		dp[1] = stair[1];
+		dp[2] = stair[1] + stair[2];
 
-		for (int i = 4; i <= N; i++) {
-			dp[i] = Math.max((arr[i] + dp[i-2]),(arr[i] + arr[i-1] + dp[i-3]));
+		for (int i = 3; i <= T; i++) {
+			dp[i] = Math.max(dp[i - 2], dp[i - 3] + stair[i - 1]) + stair[i];
 		}
-		System.out.println(dp[N]);
+
+		System.out.println(dp[T]);
 	}
 }
