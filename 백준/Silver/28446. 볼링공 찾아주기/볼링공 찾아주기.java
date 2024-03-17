6ea389c2c5848ee721c135fc8c4d0f56488f057e
김현빈 <96505736+khv9786@users.noreply.github.com>
2024-03-17 16:18:28 +0900

    [Silver IV] Title: 볼링공 찾아주기, Time: 1108 ms, Memory: 182892 KB -BaekjoonHub

--- 백준/Silver/28446. 볼링공 찾아주기/볼링공 찾아주기.java
+++ 백준/Silver/28446. 볼링공 찾아주기/볼링공 찾아주기.java
@@ -1,34 +1,29 @@
 import java.io.*;
-import java.util.*;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
-public class Main { // Boj_28446_볼링공 찾아주기
-    public static int M; // 동굴 길이, 높이
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        HashMap<Integer, ArrayList<Integer>> bowl = new HashMap<>();
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int command = Integer.parseInt(st.nextToken());
-            if (command == 1) {
-                int x = Integer.parseInt(st.nextToken());
-                int w = Integer.parseInt(st.nextToken());
-                bowl.put(w,new ArrayList<>());
-                bowl.get(w).add(x);
-            } else if(command == 2){
-                int w = Integer.parseInt(st.nextToken());
-                ArrayList<Integer> positions = bowl.get(w);
-                if (positions != null) {
-                    for (int pos : positions) {
-                        sb.append(pos).append("\n");
-                    }
-                }
+        int M = Integer.parseInt(br.readLine());
+
+        HashMap<Integer, Integer> locker = new HashMap<>();
+        for(int i=0; i<M; i++) {
+            StringTokenizer tk = new StringTokenizer(br.readLine(), " ");
+            int command = Integer.parseInt(tk.nextToken());
+
+            if(command == 1) {
+                int index = Integer.parseInt(tk.nextToken());
+                int w = Integer.parseInt(tk.nextToken());
+                locker.put(w, index);
+            } else {
+                int w = Integer.parseInt(tk.nextToken());
+                bw.write(locker.get(w) + "\n");
             }
         }
-        System.out.println(sb);
+        bw.flush();
+        bw.close();
     }
 }
