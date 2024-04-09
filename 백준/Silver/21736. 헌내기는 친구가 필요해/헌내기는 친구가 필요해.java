3dfc9ba5a6ecd131056fc63f294235780752583f
김현빈 <96505736+khv9786@users.noreply.github.com>
2024-04-09 15:23:33 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 332 ms, Memory: 41400 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -1,13 +1,13 @@
 import java.io.*;
 import java.util.*;
 
-public class Main { // Boj_21736_헌내기는 친구가 필요해
-    static int N, M, answer, start_x, start_y;
-    static int[] dx = {0,0,-1,1};
-    static int[] dy = {1,-1,0,0};
-    static char[][] arr;
-    static boolean[][] visit;
-    static final String sad_Message = "TT";
+public class Main {
+    static int N, M;
+    static char[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int result = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,51 +15,41 @@ public class Main { // Boj_21736_헌내기는 친구가 필요해
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        arr = new char[N][M];
+        map = new char[N][M];
+        visited = new boolean[N][M];
+        int dr = 0; // 도연의 위치 저장
+        int dc = 0;
+
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
             for (int j = 0; j < M; j++) {
-                arr[i][j] = str.charAt(j);
-                if(arr[i][j] == 'I'){
-                    start_x = i;
-                    start_y = j;
+                map[i][j] = str.charAt(j);
+                if (map[i][j] == 'I') {
+                    dr = i;
+                    dc = j;
                 }
             }
         }
-        bfs(start_x,start_y);
-        if(answer == 0){
-            System.out.println(sad_Message);
-        }else{
-            System.out.println(answer);
+        DFS(dr, dc);
+
+        if (result == 0) {
+            System.out.println("TT");
+        } else {
+            System.out.println(result);
         }
     }
 
-    public static void bfs(int x, int y) {
-        visit = new boolean[N][M];
-        Queue<int[]> que = new LinkedList<>();
-        que.add(new int[]{x, y});
-        visit[x][y] = true;
-
-        while (!que.isEmpty()) {
-            int[] now = que.poll();
-            int now_x = now[0];
-            int now_y = now[1];
-
-            for (int i = 0; i < 4; i++) {
-                int next_x = now_x + dx[i];
-                int next_y = now_y + dy[i];
-                if (range_chk(next_x, next_y) && !visit[next_x][next_y] && arr[next_x][next_y] != 'X') {
-                    que.add(new int[]{next_x, next_y});
-                    visit[next_x][next_y] = true;
-                    if(arr[next_x][next_y] == 'P'){
-                        answer ++;
-                    }
-                }
-            }
+    public static void DFS(int x, int y) {
+        if (x < 0 || y < 0 || x >= N || y >= M || visited[x][y] || map[x][y] == 'X') {
+            return;
         }
-    }
 
-    public static boolean range_chk(int x, int y) {
-        return 0 <= x && 0 <= y && x < N && y < M;
+        visited[x][y] = true;
+
+        if (map[x][y] == 'P') result++;
+
+        for (int i = 0; i < 4; i++) {
+            DFS(x + dx[i], y + dy[i]);
+        }
     }
 }
