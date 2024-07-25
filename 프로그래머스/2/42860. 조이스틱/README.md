9dcdcfd81044d5d7e5a1e7f6c1d8b52ac30ea873
김현빈 <96505736+khv9786@users.noreply.github.com>
2024-07-25 15:51:49 +0900

    [level 2] Title: 조이스틱, Time: 0.08 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860#) 
 
 ### 성능 요약
 
-메모리: 70.8 MB, 시간: 0.06 ms
+메모리: 74.7 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 25일 15:24:41
+2024년 07월 25일 15:51:48
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -2,21 +2,20 @@ class Solution {
     public int solution(String name) {
         int cost = 0;
         int n = name.length();
-        int minMove = n - 1; // 최대 이동 횟수는 문자열의 길이 - 1
+        int minMove = n - 1; // 최대 이동 횟수
 
-        // 상하 조작 비용 계산 및 최적 좌우 이동 계산
         for (int i = 0; i < n; i++) {
             // 상하 조작 비용
             char c = name.charAt(i);
             cost += Math.min(c - 'A', 'Z' - c + 1);
 
             // 좌우 이동 비용 계산
-            int next = i + 1;
+            int next = i+1;
             while (next < n && name.charAt(next) == 'A') {
                 next++;
             }
 
-            // 현재 위치에서 좌우 이동 비용의 최소값 계산
+            // 정방향 vs 역방향 비교
             minMove = Math.min(minMove, i + n - next + Math.min(i, n - next));
         }
 
