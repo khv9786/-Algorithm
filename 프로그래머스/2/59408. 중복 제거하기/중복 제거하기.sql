f696dc9dd5a95b76e852bd2daca7a6bcbbeb8729
김현빈 <96505736+khv9786@users.noreply.github.com>
2024-07-29 17:25:45 +0900

    [level 2] Title: 중복 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
@@ -0,0 +1,10 @@
+-- DISTINCT, GROUP BY -> 정렬 유무 차이 간단하므로 DISTINCT 사용
+-- COUNT와 DUSTUNCT로 중복 제거 후 개수 새기
+-- AS로 COUNT 별칭 주기
+
+
+SELECT COUNT (DISTINCT(NAME)) as count
+
+FROM ANIMAL_INS
+
+WHERE NAME IS NOT NULL
