5644de661d7b1236ac3d28974eafed8d8d7f0709
김현빈 <96505736+khv9786@users.noreply.github.com>
2024-08-05 15:55:57 +0900

    [level 2] Title: 할인 행사, Time: 43.67 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,53 +1,34 @@
-import java.util.*;
 
-public class Solution {
-
+class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
+        int[] N = new int[want.length];
+        int q = 0;
 
-        Map<String, Integer> wantMap = new HashMap<>();
-        for (int i = 0; i < want.length; i++) {
-            wantMap.put(want[i], number[i]);
-        }
-
-        // 이미 있다면 +1
-        Map<String, Integer> discountMap = new HashMap<>();
-        for (int i = 0; i < 10; i++) {
-            discountMap.put(discount[i], discountMap.getOrDefault(discount[i], 0) + 1);
-        }
-
-        if (matches(wantMap, discountMap)) {
-            answer++;
-        }
-
-        // 하나씩 오른쪽으로 옮기면서 검사하기.
-        for (int i = 10; i < discount.length; i++) {
-            String oldItem = discount[i - 10];
-            String newItem = discount[i];
-
-            discountMap.put(oldItem, discountMap.get(oldItem) - 1);
-            if (discountMap.get(oldItem) == 0) {
-                discountMap.remove(oldItem);
+        while(q < discount.length-9){
+            for (int k =0;k<want.length;k++){
+               N[k] = number[k];
             }
 
-            discountMap.put(newItem, discountMap.getOrDefault(newItem, 0) + 1);
-
-            // want와 비교
-            if (matches(wantMap, discountMap)) {
-                answer++;
+            for (int i = 0;i<10;i++){
+                for (int j = 0 ; j<want.length;j++){
+                   if(want[j].equals(discount[i+q])){
+                    N[j]--;
+                    break;
+                    }
+                }
             }
-        }
 
+            for (int k =0;k<want.length;k++){
+                if(N[k] != 0){
+                   break;
+                }
+                if (k==want.length-1){
+                    answer++;
+                }
+            }
+            q++;
+        }
         return answer;
     }
-
-    private boolean matches(Map<String, Integer> wantMap, Map<String, Integer> discountMap) {
-        for (String key : wantMap.keySet()) {
-            if (discountMap.getOrDefault(key, 0) < wantMap.get(key)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
 }
