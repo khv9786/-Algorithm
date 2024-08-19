2bb24d472c58e26a605bc226863b49b2ed7be248
김현빈 <96505736+khv9786@users.noreply.github.com>
2024-08-19 15:31:43 +0900

    [Silver V] Title: 집합, Time: 1016 ms, Memory: 314472 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,44 +1,37 @@
 import java.io.*;
 import java.util.*;
 
-public class Main { // 집합
+public class Main { // 집합 - 비트마스킹
     public static int M;
-    public static Set<Integer> set = new HashSet<>();
+    public static int bitSet = 0; 
     public static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         M = Integer.parseInt(br.readLine());
+
         for (int i = 0; i < M; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             String cmd = st.nextToken();
 
             if (cmd.equals("all")) {
-                for (int j = 1; j <= 20; j++) {
-                    set.add(j);
-                }
+                bitSet = (1 << 20) - 1; 
             } else if (cmd.equals("empty")) {
-                set.clear();
+                bitSet = 0;
             } else {
-                int x = Integer.parseInt(st.nextToken());
-
+                int x = Integer.parseInt(st.nextToken()) - 1; // 19까지 사용
                 switch (cmd) {
                     case "add":
-                        set.add(x);
+                        bitSet |= (1 << x);
                         break;
                     case "remove":
-                        set.remove(x);
+                        bitSet &= ~(1 << x);
                         break;
                     case "check":
-                        sb.append(set.contains(x) ? "1\n" : "0\n");
+                        sb.append((bitSet & (1 << x)) != 0 ? "1\n" : "0\n");
                         break;
                     case "toggle":
-                        if (set.contains(x)) {
-                            set.remove(x);
-                        } else {
-                            set.add(x);
-                        }
+                        bitSet ^= (1 << x);
                         break;
                 }
             }
